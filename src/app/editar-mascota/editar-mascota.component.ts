59bedbf198f0febaa8efab4e79f759ecf745a329
Luis Cabrera Benito <parzibyte@gmail.com>
2019-11-06 18:05:22 -0600

    Mostrando snackbar de actualizar mascota

--- src/app/editar-mascota/editar-mascota.component.ts
+++ src/app/editar-mascota/editar-mascota.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MascotasService } from "../mascotas.service"
 import { Mascota } from '../mascota';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-editar-mascota',
   templateUrl: './editar-mascota.component.html',
@@ -12,7 +13,8 @@ export class EditarMascotaComponent implements OnInit {
   private mascota: Mascota = new Mascota("", "", 0);
 
   constructor(private route: ActivatedRoute,
-    private router: Router, private mascotasService: MascotasService) { }
+    private router: Router, private mascotasService: MascotasService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     let idMascota = this.route.snapshot.paramMap.get("id");
@@ -25,6 +27,9 @@ export class EditarMascotaComponent implements OnInit {
 
   onSubmit() {
     this.mascotasService.updateMascota(this.mascota).subscribe(() => {
+      this.snackBar.open('Mascota actualizada', undefined, {
+        duration: 1500,
+      });
       this.volver();
     });
   }
