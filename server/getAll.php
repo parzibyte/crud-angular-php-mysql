53cefcc66e0e3ca80b25688e2ea8f8bbf800fdba
Luis Cabrera Benito <parzibyte@gmail.com>
2019-11-08 09:55:13 -0600

    Créditos ._.

--- server/getAll.php
+++ server/getAll.php
@@ -1,4 +1,22 @@
 <?php
+/*
+
+    Programado por Luis Cabrera Benito 
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+    
+    Blog:       https://parzibyte.me/blog
+    Ayuda:      https://parzibyte.me/blog/contrataciones-ayuda/
+    Contacto:   https://parzibyte.me/blog/contacto/
+*/ ?>
+<?php
 header("Access-Control-Allow-Origin: http://localhost:4200");
 $bd = include_once "bd.php";
 $sentencia = $bd->query("select id, nombre, raza, edad from mascotas");
